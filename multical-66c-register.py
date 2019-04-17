1da38ae7025ceaa2ecb7853d72af383d2cebcbf5
Jorik Jonker <jorik@kippendief.biz>
2019-04-17 22:03:35 +0200

    Add snippets to extract info from multical 66c
    It was kind of tough to reverse engineer/collect info on this, so I am
    keeping this in SCM.

---
+++ multical-66c-register.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+import serial_asyncio
+
+async def main():
+	print('Creating reader/writer')
+	reader, writer = await serial_asyncio.open_serial_connection(url='/dev/kamstrup', baudrate=300, bytesize=7, parity='E', stopbits=2)
+	data = await kamstrup66c(reader, writer)
+	print(data)
+
+async def kamstrup66c(reader, writer):
+	print('Requesting register 1')
+	writer.write(b'/#1\r\n')
+	print('Waiting')
+	await asyncio.sleep(1)
+	print('Setting baud to 1200')
+	writer.transport.serial.baudrate = 1200
+	print('Waiting')
+	await asyncio.sleep(9)
+	data = await reader.read(87)
+	print('Setting baud to 300')
+	writer.transport.serial.baudrate = 300
+	data = data[5:].decode('ascii').split(' ')
+	return {
+		'energy': float(data[0])/1000,
+		'volume': float(data[1])/1000,
+		'hours': int(data[2]),
+		't_in': float(data[3])/100,
+		't_out': float(data[4])/100,
+	}
+
+if __name__ == '__main__':
+	asyncio.run(main())
