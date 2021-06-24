e92d10a109066d495eb83a4fca9ff4dec07384ae
Jorik Jonker <jorik@kippendief.biz>
2021-06-24 14:16:34 +0200

    feat: log to stderr instead of stdout
    This helps with buffering issues in containerized environments, as
    stderr is never buffered

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import attr
 import config
 
@@ -11,17 +12,11 @@ class Measurement:
 @attr.s
 class PyNuts:
 	cfg = attr.ib()
-	log = attr.ib(init=False)
+	log = attr.ib()
 	input = attr.ib(init=False)
 	output = attr.ib(init=False)
 
 	def init(self):
-		ch = logging.StreamHandler()
-		ch.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s:%(name)s: %(message)s"))
-		self.log = logging.getLogger('pynuts')
-		self.log.addHandler(ch)
-		self.log.setLevel(self.cfg.loglevel)
-		
 		self.input = self.cfg.input_cls(self.cfg.input_name, self.log, self.cfg)
 		self.output = self.cfg.output_cls(self.cfg.output_name, self.log, self.cfg)
 
@@ -34,6 +29,14 @@ class PyNuts:
 			self.input.wait()
 
 if __name__ == '__main__':
-	p = PyNuts(config.PyNuts.from_environ())
+	cfg = config.PyNuts.from_environ()
+
+	ch = logging.StreamHandler(sys.stderr)
+	ch.setFormatter(logging.Formatter("%(asctime)s %(levelname)8s:%(name)s: %(message)s"))
+	log = logging.getLogger('pynuts')
+	log.addHandler(ch)
+	log.setLevel(cfg.loglevel)
+		
+	p = PyNuts(cfg, log)
 	p.init()
 	p.run()
