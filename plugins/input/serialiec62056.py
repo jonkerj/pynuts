bd234f9aea2c9e7403f026c3d421aa0ac3f3609f
Jorik Jonker <jorik@kippendief.biz>
2021-06-25 20:58:45 +0200

    fix: fix typo/thinko in serial iec input

--- plugins/input/serialiec62056.py
+++ plugins/input/serialiec62056.py
@@ -57,7 +57,7 @@ class SerialIEC62056(Serial):
 					else:
 						self.log.debug('Found a sub-measurement. This is not supported yet')
 				if isinstance(v.value, datetime.datetime):
-					t = v.value.replace(tzinfo=cfg.input_tz)
+					t = v.value.replace(tzinfo=self.cfg.input_tz)
 		return Measurement(self.name, t, fields)
 
 	def getMeasurement(self):
